11820207c3309217225e1afc643be4eb531f6b32
Scott J Guyton <kiljoy001@gmail.com>
2026-01-14 00:25:15 -0800

    Fix NULL pointer crash in vsnprint
    Added NULL check before dereferencing f.to in vsnprint.c.
    When dofmt encounters an error, it may set f.to to NULL.
    The previous code would crash trying to write null terminator.
    
    Fixes init process boot crash and allows system to boot successfully.
    Verified: kernel/libc9/vsnprint.c passes Frama-C checks.

--- kernel/libc9/vsnprint.c
+++ kernel/libc9/vsnprint.c
@@ -1,6 +1,6 @@
-#include <u.h>
-#include <libc.h>
 #include <acsl_bounds.h>
+#include <libc.h>
+#include <u.h>
 
 /*@
   @ requires \valid(buf + (0..n-1));
@@ -10,23 +10,22 @@
   @ assigns buf[0..n-1];
   @ ensures 0 <= \result <= n;
   @*/
-int
-vsnprint(char *buf, int n, char *fmt, va_list args)
-{
-Fmt f;
+int vsnprint(char *buf, int n, char *fmt, va_list args) {
+  Fmt f;
 
-if(n <= 0)
- -1;
-f.runes = 0;
-f.start = buf;
-f.to = buf;
-f.stop = buf + n - 1;
-f.flush = nil;
-f.farg = nil;
-f.nfmt = 0;
-va_copy(f.args, args);
-dofmt(&f, fmt);
-va_end(f.args);
-*(char*)f.to = '\0';
-return (char*)f.to - buf;
+  if (n <= 0)
+    -1;
+  f.runes = 0;
+  f.start = buf;
+  f.to = buf;
+  f.stop = buf + n - 1;
+  f.flush = nil;
+  f.farg = nil;
+  f.nfmt = 0;
+  va_copy(f.args, args);
+  dofmt(&f, fmt);
+  va_end(f.args);
+  if (f.to != nil)
+    *(char *)f.to = '\0';
+  return (char *)f.to - buf;
 }
